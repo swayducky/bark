1882198c2242b6262a5bef948859fbe223636756
Sway Ducky <swayducky@gmail.com>
2023-05-29 04:19:42 -0700

    working poetry -- by combining with llmhack requirements

---
+++ main.py
@@ -0,0 +1,18 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+from IPython.display import Audio
+
+# download and load all models
+preload_models()
+
+# generate audio from text
+text_prompt = """
+GIRL: Hey you make me want to cum so badly, I want you baby. [sigh] Oh my god. I'm gonna cum for you.
+"""
+audio_array = generate_audio(text_prompt)
+
+# save audio to disk
+write_wav("bark_generation.wav", SAMPLE_RATE, audio_array)
+  
+# play text in notebook
+Audio(audio_array, rate=SAMPLE_RATE)
